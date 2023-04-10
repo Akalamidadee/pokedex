23bcd42ce16d8dad5fae509fc9853499571fa077
Bonfim Rodrigues da silva junior <118739283+Akalamidadee@users.noreply.github.com>
2023-04-10 15:17:21 -0300

    Add files via upload

---
+++ assets/js/barra.js
@@ -0,0 +1,11 @@
+const content = document.querySelector(".content");
+const inputSearch = document.querySelector("input[type='search']");
+
+inputSearch.oninput = () => {
+    content.innerHTML = "";
+  
+    
+  };
+  
+
+  
